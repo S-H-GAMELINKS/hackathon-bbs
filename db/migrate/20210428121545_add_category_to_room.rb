52115d58e8e5d68dd2bad58c72b8bfd8d414621e
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-04-28 21:16:46 +0900

    Allow null

--- db/migrate/20210428121545_add_category_to_room.rb
+++ db/migrate/20210428121545_add_category_to_room.rb
@@ -1,5 +1,5 @@
 class AddCategoryToRoom < ActiveRecord::Migration[6.1]
   def change
-    add_reference :rooms, :category, null: false, foreign_key: true
+    add_reference :rooms, :category, null: true, foreign_key: true
   end
 end
