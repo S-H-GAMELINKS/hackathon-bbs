08f207802ba65d08888d1d32e8777f366206b1b4
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-05-08 15:35:28 +0900

    add has_rich_text

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  has_rich_text :content
   broadcasts_to :room
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ApplicationRecord
     belongs_to :user
     has_many :messages
+    has_rich_text :content
     broadcasts_to ->(room) { :rooms }
 end
