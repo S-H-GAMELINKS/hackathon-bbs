2540b9d12e77448b4d6028fda01bd49628655bcd
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-05-10 21:57:38 +0900

    Add validates for Room

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,7 @@ class Room < ApplicationRecord
     has_many :messages
     has_rich_text :content
     broadcasts_to ->(room) { :rooms }
+
+    validates :name, presence: true, length: { maximum: 30 }
+    validates :content, presence: true
 end
